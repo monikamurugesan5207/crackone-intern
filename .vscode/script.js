d9b2c0bb320e871337ed8b48e3deedd8b96dde86
Monika M <monikamurugesan5207@gmail.com>
2026-06-11 16:48:33 +0530

    My Portfolio Website

---
+++ .vscode/script.js
@@ -0,0 +1,17 @@
+let text = "Frontend Developer";
+
+let i = 0;
+
+function typing(){
+
+    if(i < text.length){
+
+        document.querySelector(".typing").innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typing,100);
+    }
+}
+
+typing();
